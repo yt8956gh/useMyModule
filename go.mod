ab87fd6ccc7de36882d6e223a4c2e4929ee0148c
Michael Mao <yt8956789.cs09g@nctu.edu.tw>
2021-01-14 02:56:33 +0000

    init

---
+++ go.mod
@@ -0,0 +1,5 @@
+module useMyModule
+
+go 1.14
+
+require github.com/yt8956gh/testGoModule v1.0.2
---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"github.com/yt8956gh/testGoModule"
+)
+
+func main() {
+	mymodule.Test() //print test
+}
